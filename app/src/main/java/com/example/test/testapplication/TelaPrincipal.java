052c071c0a312f1117e7eafc90c8572c84da9e48
Cid <cid@tjgo.jus.br>
2017-05-04 10:56:21 -0300

    criando um botao com mensagem

--- app/src/main/java/com/example/test/testapplication/TelaPrincipal.java
+++ app/src/main/java/com/example/test/testapplication/TelaPrincipal.java
@@ -2,6 +2,9 @@ package com.example.test.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaPrincipal extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        Button btn = (Button) findViewById(R.id.btnOK);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Meu App", "Esta eh uma mensagem de Teste");
+            }
+        });
     }
 }
